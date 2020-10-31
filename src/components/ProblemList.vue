9a808057a616ee238862ea6ff06eba6c912724bd
F9Uf <atoseka@gmail.com>
2020-11-01 05:23:29 +0700

    create problem success

--- src/components/ProblemList.vue
+++ src/components/ProblemList.vue
@@ -9,13 +9,13 @@
         cols="12"
       >
         <v-img
-          src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZWuVzZ1VpsxVHoX7HFpRD8Y0RxP3VZczPzrbdaSevF5glfzk3fv3.jpg" height="220px"
+          :src="image" height="220px"
         ></v-img>
       </v-col>
     </v-row>
     <v-card-title class="align-start">
       <div>
-        <span class="headline">#ถนนไม่ควรเป็นแบบนี้</span>
+        <span class="headline">{{ title }}</span>
       </div>
       <v-spacer></v-spacer>
     </v-card-title>
@@ -23,7 +23,7 @@
     <v-card-actions>
     </v-card-actions>
     <div class="pa-4 pt-0 pb-0 caption" style="text-align: left">
-      <em>ถนนไม่ควรที่จะเป็นแบบนี้ ถนนที่นี่พุพังมาก ไม่มีใครซ่อมมานานแรมปีแล้ว อยากให้หน่วยงานที่เกี่ยวข้อง หรือภาคเอกชน ช่วยมาดูแลจัดการให้ที ชาวบ้านที่ใช้เส้นทางนี้ในการเดินทางสรรจรลำบากมาก</em>
+      <em>{{ description }}</em>
     </div>
     <!-- <v-divider></v-divider> -->
     <v-text-field
@@ -33,18 +33,15 @@
     >
     </v-text-field>
     <v-list>
-      <v-list-item v-for="item in items" :key="item">
+      <v-list-item v-for="item in solutions" :key="item.id">
         <v-list-item-avatar>
             <v-img :src="item.avatar"></v-img>
           </v-list-item-avatar>
         <v-list-item-content style="text-align: left;">
           <v-list-item-title>{{ item.name }}</v-list-item-title>
-          <v-list-item-subtitle>
-            <span class="text--primary">{{ item.description}}1</span>
-          </v-list-item-subtitle>
+            <em class="text--primary caption">{{ item.description}}</em>
         </v-list-item-content>
       </v-list-item>
-      <v-divider :inset="true"></v-divider>
     </v-list>
   </v-card>
 </template>
@@ -54,17 +51,13 @@ export default {
   data() {
     return {
       comment: '',
-      items: [
-        { id: '1', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', name: 'Community1',  description: 'test' },
-        { id: '2', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', name: 'Community2',  description: 'test' },
-        { id: '3', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', name: 'Community3',  description: 'test' },
-        { id: '4', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg', name: 'Community4',  description: 'test' },
-      ]
     }
   },
   props: {
     title: String,
-    rating: Number
+    image: String,
+    description: String,
+    solutions: Array
   }
 }
 </script>
--- src/views/Problem.vue
+++ src/views/Problem.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="problem-page">
-    <ProblemList />
+    <ProblemList
+      v-for="(p, i) in problems"
+      :key="i"
+      :image="p.image"
+      :title="p.title"
+      :description="p.description"
+      :solutions="p.solutions"
+    />
   </div>
 </template>
 
@@ -10,6 +17,32 @@ import ProblemList from '../components/ProblemList.vue';
 export default {
   components: {
     ProblemList
-  }  
+  },
+  data() {
+    return {
+      problems: [
+        {
+          title: '#ถนนไม่ควรเป็นแบบนี้',
+          image: 'https://www.thairath.co.th/media/dFQROr7oWzulq5FZWuVzZ1VpsxVHoX7HFpRD8Y0RxP3VZczPzrbdaSevF5glfzk3fv3.jpg',
+          description: 'ถนนไม่ควรที่จะเป็นแบบนี้ ถนนที่นี่พุพังมาก ไม่มีใครซ่อมมานานแรมปีแล้ว อยากให้หน่วยงานที่เกี่ยวข้อง หรือภาคเอกชน ช่วยมาดูแลจัดการให้ที ชาวบ้านที่ใช้เส้นทางนี้ในการเดินทางสรรจรลำบากมาก',
+          solutions: [
+            { id: '1', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', name: 'รักถนน',  description: 'พวกเราจะใช้ทราย 20 กระสอบ กับปูนตราเสือดาว 10 กระสอบ บรรจงเทลงอย่างตั้งใจ พร้อมด้วยทีมงาน 7คน' },
+            { id: '2', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', name: 'บริษัท SCG',  description: 'จะใช้วิธีการถมปูนใหม่ทั้งหมด เพื่อที่จะให้ถนนมีระดับเท่ากัน' },
+          ]
+        },
+        {
+          title: '#เด็กๆควรมีชีวิตที่ดีกว่านี้',
+          image: 'https://transbordernews.in.th/home/wp-content/uploads/17521954_1383866081656673_2081395115_o.jpg',
+          description: 'น้องๆ เหล่านี้น่าสงสารมาก พวกเขาไม่ได้มีโอกาสในการเรียนหนังสือเทียบเท่ากับพวกเรา ซึ่งการศึกษาถือเป็นสิ่งที่จำเป็นมากต่อการประกอบอาชีพ หรือ การเลี้ยงดูตนเองในอนาคต ดังนั้นพวกเราควรมาช่วยกันทำให้เด็กเหล่านี้มีการศึกษาที่ดีขึ้นกันเถอะ',
+          solutions: [
+            { id: '1', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', name: 'Community1',  description: 'test' },
+            { id: '2', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', name: 'Community2',  description: 'test' },
+            { id: '3', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', name: 'Community3',  description: 'test' },
+            { id: '4', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg', name: 'Community4',  description: 'test' },
+          ]
+        }
+      ]
+    }
+  } 
 }
 </script>
