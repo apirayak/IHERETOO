e837f09cb5fdddf8ed0ce1cfa3c28df2525630a1
styley997 <styley997@gmail.com>
2020-10-31 14:33:39 +0700

    add vuetify router googlemap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCT-JOXW4wZDGE8rmbyeFJ3fNgozHqpzlQ',
+    libraries: 'places',
+  },
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+
+  components: {
+    
+  },
+
+  data() {
+    return {
+    }
+  }
+}
+</script>
